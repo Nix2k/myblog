b49c44ba75b3a83612b19a346fc1b4538b6da742
Nix2k <mihail.bokov@gmail.com>
2018-09-21 23:52:28 +0300

    v2.6

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,35 +1,35 @@
 <?/* 
-    Шаблон страницы с перечнем статей
-    В него необходимо передать массив статей
+	Шаблон страницы с перечнем статей
+	В него необходимо передать массив статей
 */?>
 
 @extends('layouts.master')
 
 @section('main')
-    <main>
-        <section id="blog">
-            <div class="container">
-                <h3>Блог</h3>
-                @foreach ($posts as $post)
-                    <article>
-                        <h4>{{ $post->title }}</h4>
-                        <div class="content">
-                            {!! strip_tags(substr($post->text, 0, 300), '<p><i><b><u><span><strong>') !!}...
-                        </div>
-                        <div class="links">
-                            <a href="/post/{{ $post->id }}">Подробнее</a>
-                            <p> Опубликовано {{ $post->updated_at }} </p>
-                        </div>
-                    </article>
-                @endforeach
-                <div class="links">
-                    <p>Страницы:
-                        @for ($i = 1; $i < count($posts); $i++)
-                            <a href="/?page={{ $i }}">{{ $i }}</a>
-                        @endfor
-                    </p>
-                </div>
-            </div>
-        </section>
-    </main>
+	<main>
+		<section id="blog">
+			<div class="container">
+				<h3>Блог</h3>
+				@foreach ($posts as $post)
+					<article>
+						<h4>{{ $post->title }}</h4>
+						<div class="content">
+							{!! strip_tags(substr($post->text, 0, 300), '<p><i><b><u><span><strong>') !!}...
+						</div>
+						<div class="links">
+							<a href="/post/{{ $post->id }}">Подробнее</a>
+							<p> Опубликовано {{ $post->updated_at }} </p>
+						</div>
+					</article>
+				@endforeach
+				<div class="links">
+					<p>Страницы:
+						@for ($i = 1; $i < count($posts)/5+1; $i++)
+							<a href="/?page={{ $i }}">{{ $i }}</a>
+						@endfor
+					</p>
+				</div>
+			</div>
+		</section>
+	</main>
 @endsection
