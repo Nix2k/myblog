53c244312b2350056b53e7a298569cb9ba829ebc
Nix2k <mihail.bokov@gmail.com>
2018-09-21 23:06:15 +0300

    v2.4

--- app/Http/Controllers/CMessage.php
+++ app/Http/Controllers/CMessage.php
@@ -23,7 +23,7 @@ class CMessage extends Controller
 
 	//Добавить сообщение в БД и отправить e-mail
 	public static function add() {
-		$message = new Message;
+		$message = new Message; //Создание записи в БД
 		$message->subject = strip_tags(htmlspecialchars($_POST['subject']));
 		$message->name = strip_tags(htmlspecialchars($_POST['name']));
 		$message->text = strip_tags(htmlspecialchars($_POST['message']));
@@ -34,14 +34,14 @@ class CMessage extends Controller
 			'email' => $message->email,
 			'text' => $message->text
 		];
-		if (($_FILES['file']['size'] > 0)&&($_FILES['file']['size'] <= 5000000)) {
+		if (($_FILES['file']['size'] > 0)&&($_FILES['file']['size'] <= 5000000)) { //Загрузка файла
 			$uploadDir = '/var/www/html/myblog/storage/app/public';
 			$fName = round(rand(10000, 99999)) . basename($_FILES['file']['name']);
 			$message->file = $fName;
 			$uploadFile = $uploadDir . $fName;
-			move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile);
+			move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile);
 		}
-		Mail::send('emails.message', $msgParams, function($msg)
+		Mail::send('emails.message', $msgParams, function($msg) //Отправка сообщения
 		{
 		    $msg->to('mihail.bokov@gmail.com')->subject('Новое сообщение в блоге');
 		});
